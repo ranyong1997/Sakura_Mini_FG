6942ee91a669d7500d0f973392ab9bb3804e26b3
ranyong <ranyong1209@gmail.com>
2023-03-03 14:10:27 +0800

    feat: 🚀 新增后端微服务端口名若干

--- src/api/config/servicePort.ts
+++ src/api/config/servicePort.ts
@@ -1,3 +1,9 @@
 // * 后端微服务端口名
 export const PORT1 = "/geeker";
 export const PORT2 = "/hooks";
+export const PORT3 = "/co";
+export const PORT4 = "/ca";
+export const PORT5 = "/role";
+export const PORT6 = "/user";
+export const PORT7 = "/password";
+export const PORT8 = "/test";
